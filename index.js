f12ee2aee0ac3ee744eb3ecfcca4083fbd4174b8
famedirtyshit <famedirtyshit@gmail.com>
2021-08-21 21:56:30 +0700

    change: function index res

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: false }));
 firebaseInit();
 
 app.get(`/`, (req, res) => {
-    res.send(`Hello World from Cloud Express.js`);
+    res.send(`Hello World Express.js`);
 })
 
 app.use(`/postLostCat`, postLostCatRouter);
