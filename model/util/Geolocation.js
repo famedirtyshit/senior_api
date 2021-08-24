17a663589e37e7c51edb582753ad0533587a4647
famedirtyshit <famedirtyshit@gmail.com>
2021-08-25 02:33:22 +0700

    add: geo util

--- model/util/Geolocation.js
+++ model/util/Geolocation.js
@@ -1,20 +1,20 @@
-// const geolib = require('geolib');
+const geolib = require('geolib');
 
-// const checkGeolocation = (srcLat, srcLng, desLat, desLng) => {
-//     return geolib.getDistance(
-//         { latitude: srcLat, longitude: srcLng },
-//         { latitude: desLat, longitude: desLng }
-//     );
-// }
+const checkGeolocation = (srcLat, srcLng, desLat, desLng) => {
+    return geolib.getDistance(
+        { latitude: srcLat, longitude: srcLng },
+        { latitude: desLat, longitude: desLng }
+    );
+}
 
-// const sortByGeo = (a, b) => {
-//     if (a.distance < b.distance) {
-//         return -1;
-//     }
-//     if (a.distance > b.distance) {
-//         return 1;
-//     }
-//     return 0;
-// }
+const sortByGeo = (a, b) => {
+    if (a.distance < b.distance) {
+        return -1;
+    }
+    if (a.distance > b.distance) {
+        return 1;
+    }
+    return 0;
+}
 
-// module.exports = { checkGeolocation, sortByGeo }
+module.exports = { checkGeolocation, sortByGeo }
